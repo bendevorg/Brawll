52d1f0965b0a6979f175e5493d6f92b01d1305d9
GuiMac <guibasconti@gmail.com>
2017-04-22 03:12:26 -0300

    Game controller with all entities position

--- Assets/Scripts/ArtificialIntelligence.cs
+++ Assets/Scripts/ArtificialIntelligence.cs
@@ -5,7 +5,22 @@ using UnityEngine;
 public class ArtificialIntelligence : MonoBehaviour {
 
 	public void DecideNextMovement(){
-		
+
+		float minDistance = int.MaxValue;
+
+		foreach (GameController.Player player in GameController.gameController.players) {
+
+			float playerDistance = Vector3.Distance(transform.position, player.GetPosition());
+			Debug.Log(playerDistance);
+
+			if (playerDistance < minDistance) {
+
+				minDistance = playerDistance;
+
+			}
+
+		}
+
 	}
 
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController gameController = null;
+
+	public List<Player> players = new List<Player>();
+
+	void Awake(){
+
+		if(gameController != null && gameController != this){
+
+			Destroy(this.gameObject);
+
+		} else {
+
+			gameController = this;
+
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		List<GameObject> livingEntities = new List<GameObject>();
+		livingEntities.AddRange(GameObject.FindGameObjectsWithTag("Player"));
+		livingEntities.AddRange(GameObject.FindGameObjectsWithTag("Enemy"));
+
+		foreach (GameObject livingEntity in livingEntities){
+
+			players.Add(new Player(livingEntity, livingEntity.transform.position));
+
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		foreach (Player player in players){
+
+			player.UpdatePlayerPosition();
+
+		}
+		
+	}
+
+	public struct Player{
+
+		GameObject entity;
+		Vector3 position;
+
+		public Player(GameObject _entity, Vector3 _position){
+
+			entity = _entity;
+			position = _position;
+
+		}
+
+		public void UpdatePlayerPosition(){
+			position = entity.transform.position;
+		}
+
+		public Vector3 GetPosition(){
+			return position;
+		}
+
+	}
+
+}
